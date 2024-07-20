bbdda275e0e19f168cd79e5d9b9ecb7e20db7bca
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-20 10:03:51 -0500

    Added basic vim options

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt
+
+-- line numbers
+opt.number = true
+-- opt.relativenumber = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true -- if mixed case in search then search case-sensitive
+
+-- cursor line
+opt.cursorline = true
+
+-- true color terminal
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- swap file
+opt.swapfile = false
