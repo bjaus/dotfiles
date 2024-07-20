6d55b4a64da554ebbd1b426dab8339c74eea3c9f
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-20 11:47:35 -0500

    Added file tree plugin

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  priority = 999,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local tree = require("nvim-tree")
+
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    tree.setup({
+      view = {
+        width = 40,
+        relativenumber = false,
+      },
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false,
+      },
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer to current file" })
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+  end
+}
