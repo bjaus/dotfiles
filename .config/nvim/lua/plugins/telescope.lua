f03c6193f4699d8f9a2a870b11a8b38858ffa1d8
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-20 10:40:44 -0500

    Added telescope plugin

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to previous result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+          },
+        },
+      },
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in pwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Search pattern in pwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Search string under cursor in pwd" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find TODOs" })
+  end,
+}
