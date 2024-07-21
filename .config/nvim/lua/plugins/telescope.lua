da4755b2252154662419ee00819b69d95574cead
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-21 07:34:44 -0500

    Added ui select to telescope plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,31 +1,36 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  enabled = true,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "folke/todo-comments.nvim",
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
   },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+  {
+    "nvim-telescope/telescope.nvim",
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "folke/todo-comments.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to previous result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous, -- move to previous result
+              ["<C-j>"] = actions.move_selection_next, -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
           },
         },
-      },
-    })
+      })
 
-    vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in pwd" })
-    vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-    vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Search pattern in pwd" })
-    vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Search string under cursor in pwd" })
-    vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comments" })
-  end,
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in pwd" })
+      vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Search pattern in pwd" })
+      vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Search string under cursor in pwd" })
+      vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comments" })
+    end,
+  },
 }
