f22ae28d683fca68cdbc13d3e4f54f99b113dcea
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-21 08:01:39 -0500

    Renabled cmp plugin

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,17 +1,11 @@
 return {
   "hrsh7th/nvim-cmp",
-  enabled = false,
+  enabled = true,
   event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
-    {
-      "L3MON4D3/LuaSnip",
-      -- follow latest release.
-      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "make install_jsregexp",
-    },
+    "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
     "onsails/lspkind.nvim", -- vs-code like pictograms
@@ -46,8 +40,8 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
+        { name = "nvim_lsp" }, -- language server protocol
+        { name = "luasnip" }, -- code snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
