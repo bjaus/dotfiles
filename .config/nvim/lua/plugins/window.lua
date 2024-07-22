f477c2ca460ce31079c2f047e62e0bb2f8d965aa
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-22 02:54:06 -0500

    removed custom setup

--- .config/nvim/lua/plugins/window.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "szw/vim-maximizer",
-    enabled = true,
-    keys = {
-      { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize split window" },
-    },
-  },
-}
