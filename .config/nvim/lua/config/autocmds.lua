537ab0860341a9694e47c6726c0abf0e55ffeaee
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-23 06:40:34 -0500

    reorganized into modules

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,48 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+M = {}
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+function M.setup_lsp_highlight(event)
+  -- The following autocommands are used to highlight references of the
+  -- word under the cursor when the cursor rests there for a little while.
+  -- See `:help CursorHold` for information about when this is executed
+  --
+  -- When you move the cursor, the highlights will be cleared.
+  local client = vim.lsp.get_client_by_id(event.data.client_id)
+  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+    local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      buffer = event.buf,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.document_highlight,
+    })
+
+    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      buffer = event.buf,
+      group = highlight_augroup,
+      callback = vim.lsp.buf.clear_references,
+    })
+
+    vim.api.nvim_create_autocmd('LspDetach', {
+      group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+      callback = function(event2)
+        vim.lsp.buf.clear_references()
+        vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+      end,
+    })
+  end
+end
+
+return M
