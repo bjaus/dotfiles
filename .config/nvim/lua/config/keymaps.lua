102ee4a62c9747e662d7171f11742b10d52f0ec7
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-24 06:58:52 -0500

    added git blame plugin

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -183,4 +183,15 @@ function M.setup_lsp_keymaps(event)
   end
 end
 
+function M.setup_git_blame_keymaps()
+  return {
+    -- toggle needs to be called twice; https://github.com/f-person/git-blame.nvim/issues/16
+    { '<leader>gbl', ':GitBlameToggle<CR>', desc = 'Blame line (toggle)', silent = true },
+    { '<leader>gbs', ':GitBlameCopySHA<CR>', desc = 'Copy SHA', silent = true },
+    { '<leader>gbc', ':GitBlameCopyCommitURL<CR>', desc = 'Copy commit URL', silent = true },
+    { '<leader>gbf', ':GitBlameCopyFileURL<CR>', desc = 'Copy file URL', silent = true },
+    { '<leader>gbo', ':GitBlameOpenFileURL<CR>', desc = 'Open file URL', silent = true },
+  }
+end
+
 return M
