60c212172adaae116306d99bfaf7dd70cbec6b37
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-24 14:49:02 -0500

    Moved all git plugins into same file

--- .config/nvim/lua/plugin/git.lua
+++ .config/nvim/lua/plugin/git.lua
@@ -7,4 +7,30 @@ return {
     end,
     keys = require('config.keymaps').setup_git_blame_keymaps(),
   },
+  {
+    'kdheepak/lazygit.nvim',
+    event = 'VeryLazy',
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('config.keymaps').setup_lazygit_keymaps()
+    end,
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        require('config.keymaps').setup_git_signs_keymaps(bufnr)
+      end,
+    },
+  },
 }
