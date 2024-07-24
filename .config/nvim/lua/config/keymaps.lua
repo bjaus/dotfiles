db012a27fa8e3acb54d6760854c6885845244227
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-24 14:49:02 -0500

    Moved all git plugins into same file

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -192,6 +192,57 @@ function M.setup_neotree_keymaps()
   }
 end
 
+function M.setup_git_signs_keymaps(bufnr)
+  local gitsigns = require 'gitsigns'
+
+  local function map(mode, l, r, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
+    vim.keymap.set(mode, l, r, opts)
+  end
+
+  -- Navigation
+  map('n', ']c', function()
+    if vim.wo.diff then
+      vim.cmd.normal { ']h', bang = true }
+    else
+      gitsigns.nav_hunk 'next'
+    end
+  end, { desc = 'Jump to next git [c]hange' })
+
+  map('n', '[c', function()
+    if vim.wo.diff then
+      vim.cmd.normal { '[h', bang = true }
+    else
+      gitsigns.nav_hunk 'prev'
+    end
+  end, { desc = 'Jump to previous git [c]hange' })
+
+  -- Actions
+  -- visual mode
+  map('v', '<leader>hs', function()
+    gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+  end, { desc = 'stage git hunk' })
+  map('v', '<leader>hr', function()
+    gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+  end, { desc = 'reset git hunk' })
+  -- normal mode
+  map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+  map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+  map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+  map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
+  map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+  map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+  map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+  map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+  map('n', '<leader>hD', function()
+    gitsigns.diffthis '@'
+  end, { desc = 'git [D]iff against last commit' })
+  -- Toggles
+  map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+  map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
+end
+
 function M.setup_git_blame_keymaps()
   return {
     -- toggle needs to be called twice; https://github.com/f-person/git-blame.nvim/issues/16
@@ -201,6 +252,28 @@ function M.setup_git_blame_keymaps()
     { '<leader>gbf', ':GitBlameCopyFileURL<CR>', desc = 'Copy file URL', silent = true },
     { '<leader>gbo', ':GitBlameOpenFileURL<CR>', desc = 'Open file URL', silent = true },
   }
+end
+
+function M.setup_lazygit_keymaps()
+  --   "LazyGit",
+  --   "LazyGitConfig",
+  --   "LazyGitCurrentFile",
+  --   "LazyGitFilter",
+  --   "LazyGitFilterCurrentFile",
+
+  map_normal_mode('<leader>gl', function()
+    -- -- if keymap <Esc><Esc> is set in terminal mode, remove it.
+    -- -- this is to enable <Esc> to navigate in LazyGit which otherwise
+    -- -- is overridden for terminal usage.
+    -- local terminal_keymaps = vim.api.nvim_get_keymap 't'
+    -- for _, keymap in pairs(terminal_keymaps) do
+    --   if keymap.lhs == '<Esc><Esc>' then
+    --     vim.api.nvim_del_keymap('t', '<Esc><Esc>')
+    --   end
+    -- end
+
+    vim.cmd 'LazyGit'
+  end)
 end
 
 return M
