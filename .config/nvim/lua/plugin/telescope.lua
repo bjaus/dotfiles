09d4423ef2a2924c0e26302af4c8acdf75741efd
Brandon Jaus <brandon.jaus@gmail.com>
2024-07-27 08:11:42 -0500

    Moved telescope plugin into own file

---
+++ .config/nvim/lua/plugin/telescope.lua
@@ -0,0 +1,49 @@
+return {
+  { -- Fuzzy Finder (files, lsp, etc)
+    'nvim-telescope/telescope.nvim',
+    enabled = true,
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      'nvim-telescope/telescope-ui-select.nvim',
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      'crusj/bookmarks.nvim',
+    },
+    config = function()
+      -- [[ Configure Telescope ]]
+      -- See `:help telescope` and `:help telescope.setup()`
+      require('telescope').setup {
+        -- defaults = {
+        --   mappings = {
+        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        --   },
+        -- },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+        -- path_display = 'shorten',
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
+        },
+      }
+
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+
+      require('config.keymaps').setup_telescope_keymaps()
+    end,
+  },
+}
