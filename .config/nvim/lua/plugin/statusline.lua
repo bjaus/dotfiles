38a793b75b55060ec1f7d4b953f5c210e2b35548
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-01 20:02:39 -0500

    Added lualine plugin

--- .config/nvim/lua/plugin/statusline.lua
+++ .config/nvim/lua/plugin/statusline.lua
@@ -1,10 +1,21 @@
 return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
-  opts = {
-    options = {
-      mode = "tabs",   -- only show tabpages instead of buffers
-      always_show_bufferline = false,
+  {
+    'nvim-lualine/lualine.nvim',
+    enabled = true,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      color_scheme = "cyberdream",
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    opts = {
+      options = {
+        mode = "tabs", -- only show tabpages instead of buffers
+        always_show_bufferline = false,
+      },
     },
   },
 }
