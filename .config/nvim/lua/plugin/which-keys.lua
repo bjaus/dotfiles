c79273821133b742f4421fbf67465bbca64cac25
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 10:40:32 -0500

    Added which-key keymaps back

--- .config/nvim/lua/plugin/which-keys.lua
+++ .config/nvim/lua/plugin/which-keys.lua
@@ -1,12 +1,12 @@
 return {
-    -- Useful plugin to show you pending keybindings.
-    -- https://github.com/folke/which-key.nvim
-    'folke/which-key.nvim',
-    enabled = true,
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-      vim.keymap.set('n', '<leader>wk', '<cmd>WhichKey<cr>', { desc = 'show which key'})
-      -- require('which-key').add(require('config.keymaps').setup_which_key_keymaps())
-    end,
+  -- Useful plugin to show you pending keybindings.
+  -- https://github.com/folke/which-key.nvim
+  'folke/which-key.nvim',
+  enabled = true,
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    local which_key = require 'which-key'
+    which_key.setup()
+    which_key.add(require('config.keymaps').setup_which_key())
+  end,
 }
