4d39d3b299bce55e9b290146f6a473ffdbb4f6d9
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 02:19:23 -0500

    Updated autopairs plugin

--- .config/nvim/lua/plugin/autopairs.lua
+++ .config/nvim/lua/plugin/autopairs.lua
@@ -1,11 +1,14 @@
 return {
   'windwp/nvim-autopairs',
-  enabled = true,
   event = 'InsertEnter',
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
-    require('nvim-autopairs').setup {}
-    -- If you want to automatically add `(` after selecting a function or method
+    require('nvim-autopairs').setup {
+      disable_filetype = {
+        'TelescopePrompt',
+      },
+    }
+    -- automatically add `(` after selecting a function or method
     local cmp = require 'cmp'
     local autopairs = require 'nvim-autopairs.completion.cmp'
     cmp.event:on('confirm_done', autopairs.on_confirm_done())
