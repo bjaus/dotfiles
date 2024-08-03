45adf168e892183c0cb9d39d4a88f61c80ff7d67
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 15:43:23 -0500

    Renaming and reorgnaizing plugins

---
+++ .config/nvim/lua/plugin/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/bufferline.nvim',
+  enabled = true,
+  event = 'VeryLazy',
+  opts = {
+    options = {
+      mode = 'tabs', -- only show tabpages instead of buffers
+      always_show_bufferline = false,
+    },
+  },
+}
