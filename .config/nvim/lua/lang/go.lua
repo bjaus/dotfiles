f61abff186368b3264d17902ede74611c454e8b6
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 01:23:06 -0500

    Working on setting up dap

--- .config/nvim/lua/lang/go.lua
+++ .config/nvim/lua/lang/go.lua
@@ -14,228 +14,228 @@ ls.add_snippets('go', {
 })
 
 return {
-    {
-      'nvim-neotest/neotest',
-      ft = { 'go' },
-      dependencies = {
-        'fredrikaverpil/neotest-golang',
-      },
-      opts = function(_, opts)
-        opts.adapters = opts.adapters or {}
-        opts.adapters['neotest-golang'] = {
-          go_test_args = {
-            '-cover',
-            -- '-coverfile=' .. vim.fn.getcwd() .. '/coverage.out',
-            '-json',
-            '-parallel=1',
-            '-race',
-            '-shuffle=off',
-            '-v',
-          },
-
-          -- experimental
-          -- dev_notifications = true,
-          -- runner = 'gotestsum',
-          -- gotestsum_args = { '--format=standard-verbose' },
-        }
-      end,
-    },
-    {
-      'andythigpen/nvim-coverage',
-      ft = { 'go' },
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      opts = {
-        -- https://github.com/andythigpen/nvim-coverage/blob/main/doc/nvim-coverage.txt
-        auto_reload = true,
-        lang = {
-          go = {
-            coverage_file = vim.fn.getcwd() .. '/coverage.out',
-          },
-        },
-      },
-    },
-    {
-      'mfussenegger/nvim-dap',
-      ft = { 'go' },
-      dependencies = {
-        {
-          'jay-babu/mason-nvim-dap.nvim',
-          dependencies = {
-            'williamboman/mason.nvim',
-          },
-          opts = {
-            ensure_installed = { 'delve' },
-          },
-        },
-        {
-          'leoluz/nvim-dap-go',
-          opts = {},
-        },
-      },
-      opts = {
-        configurations = {
-          go = {
-            -- See require("dap-go") source for full dlv setup.
-            {
-              type = 'go',
-              name = 'Debug test (manually enter test name)',
-              request = 'launch',
-              mode = 'test',
-              program = './${relativeFileDirname}',
-              args = function()
-                local testname = vim.fn.input 'Test name (^regexp$ ok): '
-                return { '-test.run', testname }
-              end,
-            },
-          },
-        },
-      },
-    },
-    -- {
-    --   "mfussenegger/nvim-lint",
-    --   enabled = true, -- FIXME: use lsp when possible: https://github.com/nametake/golangci-lint-langserver/issues/33
-    --   dependencies = {
-    --     {
-    --       "williamboman/mason.nvim",
-    --       opts = function(_, opts)
-    --         opts.ensure_installed = opts.ensure_installed or {}
-    --         vim.list_extend(opts.ensure_installed, { "golangci-lint" })
-    --       end,
-    --     },
-    --   },
-    --   ft = { "go", "gomod", "gowork", "gotmpl" },
-    --   opts = function(_, opts)
-    --     local args = require("lint").linters.golangcilint.args -- defaults
-    --     local config_file = require("utils.find").find_file(".golangci.yml")
-    --     if config_file then
-    --       vim.notify_once("Found file: " .. config_file, vim.log.levels.INFO)
-    --       require("utils.defaults").golangcilint_config_path = config_file
-    --       args = {
-    --         "run",
-    --         "--out-format",
-    --         "json",
-    --         "--config",
-    --         config_file,
-    --         function()
-    --           return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-    --         end,
-    --       }
-    --     end
-    --
-    --     opts.linters_by_ft["go"] = { "golangcilint" }
-    --     opts.linters["golangcilint"] = { args = args }
-    --   end,
-    -- },
-    --
-    -- {
-    --   "neovim/nvim-lspconfig",
-    --   dependencies = {
-    --     {
-    --       "williamboman/mason-lspconfig.nvim",
-    --       dependencies = {
-    --         {
-    --           "williamboman/mason.nvim",
-    --         },
-    --         {
-    --           "artemave/workspace-diagnostics.nvim",
-    --           enabled = workspace_diagnostics_enabled,
-    --         },
-    --       },
-    --       opts = function(_, opts)
-    --         opts.ensure_installed = opts.ensure_installed or {}
-    --         vim.list_extend(opts.ensure_installed, {
-    --           "gopls",
-    --
-    --           -- FIXME: https://github.com/nametake/golangci-lint-langserver/issues/33
-    --           -- "golangci_lint_ls"
-    --         })
-    --       end,
-    --     },
-    --   },
-    --   ft = { "go", "gomod", "gowork", "gotmpl" },
-    --   opts = function(_, opts)
-    --     local function golangcilint_setup()
-    --       local lspconfig = require("lspconfig")
-    --       local golangcilint_command = { "golangci-lint", "run", "--enable-all", "--out-format", "json", "--issues-exit-code=1" }
-    --       local config_file = require("utils.find").find_file(".golangci.yml")
-    --       if config_file then
-    --         vim.notify_once("Found file: " .. config_file, vim.log.levels.INFO)
-    --         golangcilint_command = { "golangci-lint", "run", "--out-format", "json", "--config", config_file, "--issues-exit-code=1" }
-    --       end
-    --
-    --       return {
-    --         golangci_lint_ls = {
-    --           -- https://github.com/nametake/golangci-lint-langserver
-    --           cmd = { "golangci-lint-langserver" },
-    --           filetypes = { "go", "gomod" },
-    --           root_dir = lspconfig.util.root_pattern("go.mod"),
-    --           init_options = {
-    --             command = golangcilint_command,
-    --           },
-    --         },
-    --       }
-    --     end
-    --
-    --     opts.servers = {
-    --
-    --       -- FIXME: https://github.com/nametake/golangci-lint-langserver/issues/33
-    --       -- golangci_lint_ls = golangcilint_setup(),
-    --
-    --       gopls = {
-    --         -- main readme: https://github.com/golang/tools/blob/master/gopls/doc/features/README.md
-    --         --
-    --         -- for all options, see:
-    --         -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
-    --         -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
-    --         -- for more details, also see:
-    --         -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
-    --         -- https://github.com/golang/tools/blob/master/gopls/README.md
-    --
-    --         on_attach = function(client, bufnr)
-    --           if workspace_diagnostics_enabled then
-    --             require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
-    --           end
-    --         end,
-    --
-    --         settings = {
-    --
-    --           gopls = {
-    --             analyses = {
-    --               fieldalignment = false, -- annoying
-    --               nilness = true,
-    --               unusedparams = true,
-    --               unusedwrite = true,
-    --               useany = true,
-    --             },
-    --             codelenses = {
-    --               gc_details = false,
-    --               generate = true,
-    --               regenerate_cgo = true,
-    --               run_govulncheck = true,
-    --               test = true,
-    --               tidy = true,
-    --               upgrade_dependency = true,
-    --               vendor = true,
-    --             },
-    --             hints = {
-    --               assignVariableTypes = true,
-    --               compositeLiteralFields = true,
-    --               compositeLiteralTypes = true,
-    --               constantValues = true,
-    --               functionTypeParameters = true,
-    --               parameterNames = true,
-    --               rangeVariableTypes = true,
-    --             },
-    --             completeUnimported = true,
-    --             directoryFilters = { "-**/node_modules", "-**/.git", "-.vscode", "-.idea", "-.vscode-test" },
-    --             gofumpt = true,
-    --             semanticTokens = true,
-    --             staticcheck = true,
-    --             usePlaceholders = true,
-    --           },
-    --         },
-    --       },
-    --     }
-    --   end,
-    -- },
+  --     {
+  --       'nvim-neotest/neotest',
+  --       ft = { 'go' },
+  --       dependencies = {
+  --         'fredrikaverpil/neotest-golang',
+  --       },
+  --       opts = function(_, opts)
+  --         opts.adapters = opts.adapters or {}
+  --         opts.adapters['neotest-golang'] = {
+  --           go_test_args = {
+  --             '-cover',
+  --             -- '-coverfile=' .. vim.fn.getcwd() .. '/coverage.out',
+  --             '-json',
+  --             '-parallel=1',
+  --             '-race',
+  --             '-shuffle=off',
+  --             '-v',
+  --           },
+  --
+  --           -- experimental
+  --           -- dev_notifications = true,
+  --           -- runner = 'gotestsum',
+  --           -- gotestsum_args = { '--format=standard-verbose' },
+  --         }
+  --       end,
+  --     },
+  --     {
+  --       'andythigpen/nvim-coverage',
+  --       ft = { 'go' },
+  --       dependencies = { 'nvim-lua/plenary.nvim' },
+  --       opts = {
+  --         -- https://github.com/andythigpen/nvim-coverage/blob/main/doc/nvim-coverage.txt
+  --         auto_reload = true,
+  --         lang = {
+  --           go = {
+  --             coverage_file = vim.fn.getcwd() .. '/coverage.out',
+  --           },
+  --         },
+  --       },
+  --     },
+  --     {
+  --       'mfussenegger/nvim-dap',
+  --       ft = { 'go' },
+  --       dependencies = {
+  --         {
+  --           'jay-babu/mason-nvim-dap.nvim',
+  --           dependencies = {
+  --             'williamboman/mason.nvim',
+  --           },
+  --           opts = {
+  --             ensure_installed = { 'delve' },
+  --           },
+  --         },
+  --         {
+  --           'leoluz/nvim-dap-go',
+  --           opts = {},
+  --         },
+  --       },
+  --       opts = {
+  --         configurations = {
+  --           go = {
+  --             -- See require("dap-go") source for full dlv setup.
+  --             {
+  --               type = 'go',
+  --               name = 'Debug test (manually enter test name)',
+  --               request = 'launch',
+  --               mode = 'test',
+  --               program = './${relativeFileDirname}',
+  --               args = function()
+  --                 local testname = vim.fn.input 'Test name (^regexp$ ok): '
+  --                 return { '-test.run', testname }
+  --               end,
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --     -- {
+  --     --   "mfussenegger/nvim-lint",
+  --     --   enabled = true, -- FIXME: use lsp when possible: https://github.com/nametake/golangci-lint-langserver/issues/33
+  --     --   dependencies = {
+  --     --     {
+  --     --       "williamboman/mason.nvim",
+  --     --       opts = function(_, opts)
+  --     --         opts.ensure_installed = opts.ensure_installed or {}
+  --     --         vim.list_extend(opts.ensure_installed, { "golangci-lint" })
+  --     --       end,
+  --     --     },
+  --     --   },
+  --     --   ft = { "go", "gomod", "gowork", "gotmpl" },
+  --     --   opts = function(_, opts)
+  --     --     local args = require("lint").linters.golangcilint.args -- defaults
+  --     --     local config_file = require("utils.find").find_file(".golangci.yml")
+  --     --     if config_file then
+  --     --       vim.notify_once("Found file: " .. config_file, vim.log.levels.INFO)
+  --     --       require("utils.defaults").golangcilint_config_path = config_file
+  --     --       args = {
+  --     --         "run",
+  --     --         "--out-format",
+  --     --         "json",
+  --     --         "--config",
+  --     --         config_file,
+  --     --         function()
+  --     --           return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+  --     --         end,
+  --     --       }
+  --     --     end
+  --     --
+  --     --     opts.linters_by_ft["go"] = { "golangcilint" }
+  --     --     opts.linters["golangcilint"] = { args = args }
+  --     --   end,
+  --     -- },
+  --     --
+  --     -- {
+  --     --   "neovim/nvim-lspconfig",
+  --     --   dependencies = {
+  --     --     {
+  --     --       "williamboman/mason-lspconfig.nvim",
+  --     --       dependencies = {
+  --     --         {
+  --     --           "williamboman/mason.nvim",
+  --     --         },
+  --     --         {
+  --     --           "artemave/workspace-diagnostics.nvim",
+  --     --           enabled = workspace_diagnostics_enabled,
+  --     --         },
+  --     --       },
+  --     --       opts = function(_, opts)
+  --     --         opts.ensure_installed = opts.ensure_installed or {}
+  --     --         vim.list_extend(opts.ensure_installed, {
+  --     --           "gopls",
+  --     --
+  --     --           -- FIXME: https://github.com/nametake/golangci-lint-langserver/issues/33
+  --     --           -- "golangci_lint_ls"
+  --     --         })
+  --     --       end,
+  --     --     },
+  --     --   },
+  --     --   ft = { "go", "gomod", "gowork", "gotmpl" },
+  --     --   opts = function(_, opts)
+  --     --     local function golangcilint_setup()
+  --     --       local lspconfig = require("lspconfig")
+  --     --       local golangcilint_command = { "golangci-lint", "run", "--enable-all", "--out-format", "json", "--issues-exit-code=1" }
+  --     --       local config_file = require("utils.find").find_file(".golangci.yml")
+  --     --       if config_file then
+  --     --         vim.notify_once("Found file: " .. config_file, vim.log.levels.INFO)
+  --     --         golangcilint_command = { "golangci-lint", "run", "--out-format", "json", "--config", config_file, "--issues-exit-code=1" }
+  --     --       end
+  --     --
+  --     --       return {
+  --     --         golangci_lint_ls = {
+  --     --           -- https://github.com/nametake/golangci-lint-langserver
+  --     --           cmd = { "golangci-lint-langserver" },
+  --     --           filetypes = { "go", "gomod" },
+  --     --           root_dir = lspconfig.util.root_pattern("go.mod"),
+  --     --           init_options = {
+  --     --             command = golangcilint_command,
+  --     --           },
+  --     --         },
+  --     --       }
+  --     --     end
+  --     --
+  --     --     opts.servers = {
+  --     --
+  --     --       -- FIXME: https://github.com/nametake/golangci-lint-langserver/issues/33
+  --     --       -- golangci_lint_ls = golangcilint_setup(),
+  --     --
+  --     --       gopls = {
+  --     --         -- main readme: https://github.com/golang/tools/blob/master/gopls/doc/features/README.md
+  --     --         --
+  --     --         -- for all options, see:
+  --     --         -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
+  --     --         -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
+  --     --         -- for more details, also see:
+  --     --         -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
+  --     --         -- https://github.com/golang/tools/blob/master/gopls/README.md
+  --     --
+  --     --         on_attach = function(client, bufnr)
+  --     --           if workspace_diagnostics_enabled then
+  --     --             require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
+  --     --           end
+  --     --         end,
+  --     --
+  --     --         settings = {
+  --     --
+  --     --           gopls = {
+  --     --             analyses = {
+  --     --               fieldalignment = false, -- annoying
+  --     --               nilness = true,
+  --     --               unusedparams = true,
+  --     --               unusedwrite = true,
+  --     --               useany = true,
+  --     --             },
+  --     --             codelenses = {
+  --     --               gc_details = false,
+  --     --               generate = true,
+  --     --               regenerate_cgo = true,
+  --     --               run_govulncheck = true,
+  --     --               test = true,
+  --     --               tidy = true,
+  --     --               upgrade_dependency = true,
+  --     --               vendor = true,
+  --     --             },
+  --     --             hints = {
+  --     --               assignVariableTypes = true,
+  --     --               compositeLiteralFields = true,
+  --     --               compositeLiteralTypes = true,
+  --     --               constantValues = true,
+  --     --               functionTypeParameters = true,
+  --     --               parameterNames = true,
+  --     --               rangeVariableTypes = true,
+  --     --             },
+  --     --             completeUnimported = true,
+  --     --             directoryFilters = { "-**/node_modules", "-**/.git", "-.vscode", "-.idea", "-.vscode-test" },
+  --     --             gofumpt = true,
+  --     --             semanticTokens = true,
+  --     --             staticcheck = true,
+  --     --             usePlaceholders = true,
+  --     --           },
+  --     --         },
+  --     --       },
+  --     --     }
+  --     --   end,
+  --     -- },
 }
--- .config/nvim/lua/plugin/formatting.lua
+++ .config/nvim/lua/plugin/formatting.lua
@@ -13,6 +13,7 @@ return {
     },
     formatters_by_ft = {
       go = { "goimports-reviser" },
+      lua = { 'stylua' },
     },
   },
   config = function(_, opts)
