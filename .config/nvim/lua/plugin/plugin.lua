b386ddef0f64390d6a85598b31d4744266b1eab2
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 16:02:43 -0500

    Added timespent plugin
    Not sure if it is useful just yet but I really like the idea of being
    able to track my time as I work.

--- .config/nvim/lua/plugin/plugin.lua
+++ .config/nvim/lua/plugin/plugin.lua
@@ -1,1 +1,10 @@
-return {}
+return { -- random plugins that don't need their own files
+  {
+    'quentingruber/timespent.nvim',
+    enabled = true,
+  },
+  {
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    enabled = true,
+  },
+}
