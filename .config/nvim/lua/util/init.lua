542714e1068707570c59c994fe4c7f7b83573c53
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 05:20:07 -0500

    Working on getting dadbod configured

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -2,13 +2,13 @@ local M = {}
 
 local function get_config_modules(excludes)
   excludes = excludes or {
-    "lazy",
-    "init",
+    'lazy',
+    'init',
   }
   local files = {}
-  local expr = vim.fn.stdpath("config") .. "/lua/config/*.lua"
+  local expr = vim.fn.stdpath 'config' .. '/lua/config/*.lua'
   for _, file in ipairs(vim.fn.glob(expr, true, true)) do
-    table.insert(files, vim.fn.fnamemodify(file, ":t:r"))
+    table.insert(files, vim.fn.fnamemodify(file, ':t:r'))
   end
   files = vim.tbl_filter(function(file)
     for _, pattern in ipairs(excludes) do
@@ -25,13 +25,13 @@ function M.load()
   for _, file in ipairs(get_config_modules()) do
     require('config.' .. file)
   end
-  require('config.lazy')
+  require 'config.lazy'
 end
 
 function M.reload_lua()
   for _, file in ipairs(get_config_modules()) do
     require('config.' .. file)
-    require('util.func')
+    require 'util.func'
   end
   vim.cmd.nohlsearch()
 end
@@ -43,6 +43,14 @@ end
 
 function M.diag_error()
   return #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }) ~= 0
+end
+
+function M.safe_require(module)
+  local status, lib = pcall(require, module)
+  if status then
+    return lib
+  end
+  return nil
 end
 
 return M
