aa4aa9f10239ebfd950d0549bc11574881996253
Brandon Jaus <brandon.jaus@gmail.com>
2024-08-03 10:25:54 -0500

    Migrated from auto-session to persistence

--- .config/nvim/lua/plugin/auto-session.lua
+++ .config/nvim/lua/plugin/auto-session.lua
@@ -1,11 +1,12 @@
 return {
   'rmagatti/auto-session',
+  enabled = false,
   lazy = false,
   dependencies = {
     'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
   },
   config = function()
-    require('auto-session').setup({
+    require('auto-session').setup {
       auto_session_use_git_branch = true,
       auto_session_suppress_dirs = {
         '/',
@@ -14,6 +15,6 @@ return {
         '~/Downloads',
         '~/Downloads',
       },
-    })
+    }
   end,
 }
---
+++ .config/nvim/lua/plugin/persistence.lua
@@ -0,0 +1,19 @@
+local config = function()
+  local persistence = require 'persistence'
+
+  persistence.setup {
+    -- dir = vim.fn.stdpath 'state' .. '/sessions/',
+    -- minimum number of file buffers that need to be open to save
+    need = 1, -- Set to 0 to always save
+    branch = true, -- use git branch to save session
+  }
+
+  require('config.keymaps').setup_persistence()
+end
+
+return {
+  'folke/persistence.nvim',
+  enabled = true,
+  event = 'BufReadPre',
+  config = config,
+}
