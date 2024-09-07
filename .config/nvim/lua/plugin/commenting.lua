bd1f51aace2589b18a2aa7e9f50e379ee68c06ff
Brandon Jaus <brandon.jaus@gmail.com>
2024-09-07 08:34:09 -0500

    Added comment context plugin

--- .config/nvim/lua/plugin/commenting.lua
+++ .config/nvim/lua/plugin/commenting.lua
@@ -1,11 +1,16 @@
 return { -- Highlight todo, notes, etc in comments
-  'folke/todo-comments.nvim',
-  enabled = true,
-  event = 'VimEnter',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
   },
-  opts = {
-    signs = false,
+  {
+    'folke/todo-comments.nvim',
+    enabled = true,
+    event = 'VimEnter',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      signs = false,
+    },
   },
 }
