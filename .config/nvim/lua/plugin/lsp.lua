24d4a577aa0a988a7a084cf688787b9ed4e1d683
Brandon Jaus <brandon.jaus@gmail.com>
2025-04-14 15:48:46 -0500

    Fixed Shift+K doc hover

--- .config/nvim/lua/plugin/lsp.lua
+++ .config/nvim/lua/plugin/lsp.lua
@@ -32,6 +32,8 @@ return {
       },
     },
     config = function()
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { focusable = false })
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
         callback = function(event)
