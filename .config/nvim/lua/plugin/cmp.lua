ac51671ec0be32d3e59a7895a9b6b625caacd023
Brandon Jaus <brandon.jaus@gmail.com>
2025-04-21 10:05:36 -0500

    Setup copilot

--- .config/nvim/lua/plugin/cmp.lua
+++ .config/nvim/lua/plugin/cmp.lua
@@ -6,37 +6,14 @@ local source_mapping = {
   buffer = '[BUF]',
   path = '[PATH]',
   treesitter = '[TREE]',
-  ['vim-dadbod-completion'] = '[DB]',
-  codeium = '[CODE]',
   dap = '[DAP]',
+  copilot = '[COP]',
 }
 
 local config = function()
   local cmp = require 'cmp'
   local lspkind = require 'lspkind'
   local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-
-  -- https://www.youtube.com/watch?v=ALGBuFLzDSA
-  -- cmp.setup.filetype({ 'sql', 'mysql', 'plsql' }, {
-  --   sources = {
-  --     { name = 'vim-dadbod-completion' },
-  --     { name = 'buffer' },
-  --   },
-  -- })
-
-  -- local autocomplete_group = vim.api.nvim_create_augroup('dadbod-autocomplete', { clear = true })
-  -- vim.api.nvim_create_autocmd('FileType', {
-  --   pattern = { 'sql', 'mysql', 'plsql' },
-  --   callback = function()
-  --     cmp.setup.buffer {
-  --       sources = {
-  --         { name = 'vim-dadbod-completion' },
-  --         { name = 'buffer' },
-  --       },
-  --     }
-  --   end,
-  --   group = autocomplete_group,
-  -- })
 
   cmp.setup {
     preselect = cmp.PreselectMode.Item,
@@ -59,7 +36,15 @@ local config = function()
       },
     },
     mapping = cmp.mapping.preset.insert(require('config.keymaps').setup_cmp()),
-    sources = {
+    sources = cmp.config.sources({
+      {
+        name = 'copilot',
+        group_index = 1,
+      },
+      {
+        name = 'nvim_lsp',
+        group_index = 1,
+      },
       {
         name = 'luasnip',
         group_index = 1,
@@ -70,35 +55,23 @@ local config = function()
         end,
       },
       {
-        name = 'nvim_lsp',
-        group_index = 2,
-      },
-      {
-        name = 'codeium',
-        group_index = 2,
-        option = { use_show_condition = true },
-        entry_filter = function()
-          return not vim.g.leetcode
-        end,
-      },
-      {
         name = 'nvim_lua',
-        group_index = 3,
+        group_index = 2,
       },
       {
         name = 'treesitter',
         keyword_length = 4,
-        group_index = 4,
+        group_index = 3,
       },
       {
         name = 'path',
         keyword_length = 4,
-        group_index = 4,
+        group_index = 3,
       },
       {
         name = 'buffer',
         keyword_length = 3,
-        group_index = 5,
+        group_index = 3,
         option = {
           get_bufnrs = function()
             local bufs = {}
@@ -112,30 +85,26 @@ local config = function()
       {
         name = 'lazydev',
         keyword_length = 2,
-        group_index = 0,
+        group_index = 4,
       },
-    },
+    }),
     ---@diagnostic disable-next-line: missing-fields
     formatting = {
       format = lspkind.cmp_format {
         mode = 'symbol_text',
         ellipsis_char = '...',
-        -- before = function(entry, vim_item)
-        --   cmp_tailwind.formatter(entry, vim_item)
-        --   return vim_item
-        -- end,
         menu = source_mapping,
       },
     },
     sorting = {
       priority_weight = 2,
       comparators = {
+        require('copilot_cmp.comparators').prioritize_tagged,
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.score,
         cmp.config.compare.recently_used,
         cmp.config.compare.kind,
-        cmp.config.compare.sort_text,
         cmp.config.compare.length,
         cmp.config.compare.order,
       },
@@ -143,13 +112,6 @@ local config = function()
   }
 
   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-  -- FIXME: How do I make this work?
-  -- cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-  --   sources = {
-  --     { name = "dap" },
-  --   },
-  -- })
 end
 
 return {
@@ -158,27 +120,35 @@ return {
   event = 'InsertEnter',
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
-    dependencies = {
-      'windwp/nvim-autopairs',
-      'L3MON4D3/LuaSnip',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lua',
-      'ray-x/cmp-treesitter',
-      'saadparwaiz1/cmp_luasnip',
-      -- "roobert/tailwindcss-colorizer-cmp.nvim",
-      'Exafunction/codeium.nvim',
-      -- {
-      --   "rcarriga/cmp-dap",
-      --   dependencies = "mfussenegger/nvim-dap",
-      -- },
-      {
-        'rafamadriz/friendly-snippets',
-        config = function()
-          require('luasnip.loaders.from_vscode').lazy_load()
-          require('luasnip.loaders.from_vscode').load { paths = { '~/.config/nvim/snip' } }
-        end,
+    'windwp/nvim-autopairs',
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lua',
+    'ray-x/cmp-treesitter',
+    'saadparwaiz1/cmp_luasnip',
+    {
+      'rafamadriz/friendly-snippets',
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+        require('luasnip.loaders.from_vscode').load { paths = { '~/.config/nvim/snip' } }
+      end,
+    },
+    {
+      'zbirenbaum/copilot.lua',
+      cmd = 'Copilot',
+      build = ':Copilot auth',
+      opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
       },
+    },
+    {
+      'zbirenbaum/copilot-cmp',
+      dependencies = { 'zbirenbaum/copilot.lua' },
+      config = function()
+        require('copilot_cmp').setup()
+      end,
     },
   },
 }
--- .config/nvim/lua/plugin/plugin.lua
+++ .config/nvim/lua/plugin/plugin.lua
@@ -1,9 +1,5 @@
 return { -- random plugins that don't need their own files
   {
-    'quentingruber/timespent.nvim',
-    enabled = true,
-  },
-  {
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
     enabled = true,
   },
