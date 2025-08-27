b0e27366c82a953c2c2c0715c81fd08787969d6a
Brandon Jaus <brandon.jaus@gmail.com>
2025-08-27 09:01:28 -0500

    feat(nvim): Restore Go package display and add breadcrumbs
    - Add lightweight cached Go package display in statusline
    - Shows package name with 📦 icon for Go files
    - Add nvim-navic for code breadcrumbs/context
    - Shows current function/method/class hierarchy
    - Breadcrumbs appear in both statusline and winbar
    - Performance optimized with caching to avoid lag

---
+++ .config/nvim/lua/plugin/breadcrumbs.lua
@@ -0,0 +1,62 @@
+return {
+  "SmiteshP/nvim-navic",
+  lazy = true,
+  dependencies = {
+    "neovim/nvim-lspconfig",
+  },
+  init = function()
+    vim.g.navic_silence = true
+  end,
+  config = function()
+    local navic = require("nvim-navic")
+    navic.setup({
+      icons = {
+        File = "󰈙 ",
+        Module = " ",
+        Namespace = "󰌗 ",
+        Package = " ",
+        Class = "󰌗 ",
+        Method = "󰆧 ",
+        Property = " ",
+        Field = " ",
+        Constructor = " ",
+        Enum = "󰕘",
+        Interface = "󰕘",
+        Function = "󰊕 ",
+        Variable = "󰆧 ",
+        Constant = "󰏿 ",
+        String = " ",
+        Number = "󰎠 ",
+        Boolean = "◩ ",
+        Array = "󰅪 ",
+        Object = "󰅩 ",
+        Key = "󰌋 ",
+        Null = "󰟢 ",
+        EnumMember = " ",
+        Struct = "󰌗 ",
+        Event = " ",
+        Operator = "󰆕 ",
+        TypeParameter = "󰊄 ",
+      },
+      lsp = {
+        auto_attach = false, -- We'll attach manually for better control
+        preference = nil,
+      },
+      highlight = false,
+      separator = " > ",
+      depth_limit = 0,
+      depth_limit_indicator = "..",
+      safe_output = true,
+    })
+
+    -- Attach navic to LSP servers
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.documentSymbolProvider then
+          navic.attach(client, args.buf)
+        end
+      end,
+    })
+  end,
+}
--- .config/nvim/lua/plugin/lualine.lua
+++ .config/nvim/lua/plugin/lualine.lua
@@ -1,10 +1,61 @@
 return {
   'nvim-lualine/lualine.nvim',
   enabled = true,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 
+    'nvim-tree/nvim-web-devicons',
+    'SmiteshP/nvim-navic',
+  },
   config = function()
     vim.cmd 'colorscheme cyberdream'
     local colors = require('util.colors').colors
+
+    -- Lightweight Go package display (cached, doesn't parse on every update)
+    local go_package_cache = {}
+    local go_package = function()
+      if vim.bo.filetype ~= 'go' then
+        return ''
+      end
+      
+      local bufnr = vim.api.nvim_get_current_buf()
+      local filepath = vim.api.nvim_buf_get_name(bufnr)
+      
+      -- Check cache first
+      if go_package_cache[filepath] then
+        return go_package_cache[filepath]
+      end
+      
+      -- Get the first few lines of the file to find package name
+      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 10, false)
+      for _, line in ipairs(lines) do
+        local package = line:match('^package%s+(%S+)')
+        if package then
+          local display = '📦 ' .. package
+          go_package_cache[filepath] = display
+          return display
+        end
+      end
+      
+      go_package_cache[filepath] = ''
+      return ''
+    end
+    
+    -- Clear cache when buffer is saved or changed
+    vim.api.nvim_create_autocmd({'BufWritePost', 'BufEnter'}, {
+      pattern = '*.go',
+      callback = function()
+        local filepath = vim.api.nvim_buf_get_name(0)
+        go_package_cache[filepath] = nil
+      end
+    })
+
+    -- Breadcrumbs component using nvim-navic
+    local breadcrumbs = function()
+      local ok, navic = pcall(require, "nvim-navic")
+      if ok and navic.is_available() then
+        return navic.get_location()
+      end
+      return ""
+    end
 
     require('lualine').setup {
       options = {
@@ -31,7 +82,7 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = { 'filename', go_package, breadcrumbs },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
@@ -39,10 +90,16 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
+        lualine_c = { 'filename', go_package },
         lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {}
+      },
+      winbar = {
+        lualine_c = { breadcrumbs },
+      },
+      inactive_winbar = {
+        lualine_c = { 'filename' },
       },
     }
   end,
