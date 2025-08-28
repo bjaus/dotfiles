f7d336e79807b1ec189f3ab249236d0dcb6f8ca5
Brandon Jaus <brandon.jaus@gmail.com>
2025-08-28 12:01:01 -0500

    fix(nvim): Fix markdown-preview.nvim build configuration
    - Change build command to use npm install directly
    - Add proper init function for filetypes
    - Configure browser and refresh settings
    - Manual npm install completed successfully

--- .config/nvim/lua/plugin/markdown-toc.lua
+++ .config/nvim/lua/plugin/markdown-toc.lua
@@ -18,13 +18,18 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
     keys = {
       { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
     },
     config = function()
       vim.g.mkdp_auto_close = 0
       vim.g.mkdp_theme = 'dark'
+      vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_browser = ''  -- Use default browser
     end,
   },
 
